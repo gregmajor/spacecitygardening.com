1e6a9399a57749980ed6ffb4a6f16aaff097f383
Greg Major <greg.b.major@gmail.com>
2022-09-16 12:02:05 -0500

    More port tweaking

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	_ "spacecitygardening.com/routers"
 	beego "github.com/beego/beego/v2/server/web"
-	// "strconv"
-	// "os"
+	"strconv"
+	"os"
 )
 
 func main() {
-	// port, err := strconv.Atoi(os.Getenv("PORT"))
-    //     if err == nil {
-    //         beego.HttpPort = port
-    //     }
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.BConfig.Listen.HTTPPort = port
+	} 
 
 	beego.SetStaticPath("/assets", "assets")
 
